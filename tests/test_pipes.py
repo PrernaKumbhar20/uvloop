3808d5624531c956e2c3c5ef2bed4ba8f3b90bca
PrernaKumbhar20 <Prerna.Kumbhar@ibm.com>
2026-04-29 13:31:59 +0530

    new commit 4

--- tests/test_pipes.py
+++ tests/test_pipes.py
@@ -249,11 +249,10 @@ class _BasePipeTest:
         proto.transport.close()
         self.loop.run_until_complete(proto.done)
         self.assertEqual('CLOSED', proto.state)
-    
     @unittest.skipIf(
         platform.machine() == 'ppc64le',
         'https://github.com/MagicStack/uvloop/issues/576')
-        
+
     def test_write_buffer_full(self):
         rpipe, wpipe = os.pipe()
         pipeobj = io.open(wpipe, 'wb', 1024)
